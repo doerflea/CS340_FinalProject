adabb955fc9825308553df0061d78eb5b8c03761
Ryan Little <ryan.jay.little@gmail.com>
2020-05-20 22:26:43 -0700

    Added a page to view catteries

--- server.js
+++ server.js
@@ -11,10 +11,23 @@ app.set('port', process.argv[2]);
 
 app.use(express.static('public'));
 
-app.get('/cattery', function (req,res){
+app.get('/', function(req, res){
+  res.status(200).render('LoginPage');
+});
+
+app.get('/cats', function (req,res){
   var context = {};
   mysql.pool.query('SELECT * FROM cat', function(err, rows, fields){
   	res.status(200).render('CatteryPage', {catData: rows});
+	});
+});
+
+app.get('/catteries', function (req,res){
+    // SQL command that grabs the cattery name, creator name, and number of cats from
+    // all catteries.
+    var sqlcommand = 'SELECT cattery.name catteryName, user.name creatorName, COUNT(cat.id) numCats FROM cattery LEFT JOIN user ON cattery.own_id = user.id JOIN cat ON cat.cattery_id=cattery.id;';
+    mysql.pool.query(sqlcommand, function(err, rows, fields){
+  	res.status(200).render('ShowCatteries', {catteryData: rows});
 	});
 });
 
