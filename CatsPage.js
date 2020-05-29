d465363b86e84bb4af0dc62c43be658d26e38ee4
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2020-05-29 16:02:08 -0700

    updating stats

--- CatsPage.js
+++ CatsPage.js
@@ -5,6 +5,18 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+  router.post('/data-feed-stat', function(req,res){
+    console.log(req.body.stat)
+    console.log(req.body.cat_id)
+    mysql.pool.query('UPDATE cat SET feed_stat =' + req.body.stat 'WHERE id' = req.body.cat_id , function(err, rows, fields){
+      if(error){
+           console.log(JSON.stringify(error))
+           res.write(JSON.stringify(error));
+           res.end();
+       }else
+  });
+  });
+
 
    router.get('/', function(req, res){
     var mysql = req.app.get('mysql');
--- public/index.js
+++ public/index.js
@@ -1,21 +1,46 @@
 var focus_variable = -1;
 var catCards = document.getElementsByClassName('cat-card');
-var last_target;
+var cat_id = -1;
 
 console.log(catCards)
 //Adds event listener which changes focus variable and background color of cat card to each cat card
 function addListToCats(){
   for(var i = 0; i < catCards.length; i++){
   catCards[i].addEventListener('click',function(event) {
-      if(focus_variable > -1){
+      if(cat_id > -1){
         last_target.style['background-color'] = "white";
-
      }
       event.currentTarget.style['background-color'] = "#ededf0";
       last_target = event.currentTarget;
-      focus_variable = 0;
+      cat_id = event.currentTarget.getAttribute("data-id");
 
 });
+
 }
 }
 window.onload = addListToCats();
+
+var feed = document.getElementById("Feed");
+feed.addEventListener('click',  function(){ stat("data-feed-stat","feed-stat-text")});
+
+/*var feed = document.getElementById("Groom");
+feed.addEventListener('click',  function(){ stat("data-groom-stat","data-groom-text" )});
+var feed = document.getElementById("Play");
+feed.addEventListener('click',  function(){ stat("data-play-stat","data-play-text" )});*/
+var xhr = new XMLHttpRequest();
+
+function stat(att,text){
+  var data_total = document.getElementById(cat_id).getAttribute(att);
+  if(data_total < 2){
+    data_total++;
+    document.getElementById(cat_id).setAttribute(att, data_total);
+    xhr.open("POST", "/CatsPage/data-feed-stat", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+    cat_id: cat_id,
+    stat: data_total
+  }));
+  console.log("post sent")
+}
+    //document.getElementById(cat_id).getElementsByClassName(text).textContent = data_total + "/2";
+}
