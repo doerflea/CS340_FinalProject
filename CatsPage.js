bf344b306f2c739878f2e0481e3507fcecd98779
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2020-05-29 14:28:57 -0700

    changing bkground color to show cat being handled

--- CatsPage.js
+++ CatsPage.js
@@ -1,12 +1,20 @@
+
+
+
 module.exports = function(){
     var express = require('express');
     var router = express.Router();
+
+
    router.get('/', function(req, res){
     var mysql = req.app.get('mysql');
      mysql.pool.query('SELECT * FROM cat WHERE owner_id =' + req.session.userID , function(err, rows, fields){
        res.status(200).render('CatsPage', {catData: rows});
      });
    });
-    return router;
 
-}();
+
+
+
+    return router;
+    }();
--- public/index.js
+++ public/index.js
@@ -1,37 +1,21 @@
+var focus_variable = -1;
+var catCards = document.getElementsByClassName('cat-card');
+var last_target;
 
-//var cat_in_focus = 0;
-
-/**
-**Function Name: addListToCats
-**Input: N/A
-**Description: Adds event listener when cat card is created which changes cat_in_focus variable
-*/
-/*
+console.log(catCards)
+//Adds event listener which changes focus variable and background color of cat card to each cat card
 function addListToCats(){
   for(var i = 0; i < catCards.length; i++){
   catCards[i].addEventListener('click',function(event) {
-      catCards[cat_in_focus].style['background-color'] = "white";
+      if(focus_variable > -1){
+        last_target.style['background-color'] = "white";
+
+     }
       event.currentTarget.style['background-color'] = "#ededf0";
-      cat_in_focus = event.currentTarget.getAttribute("data-cat-num");
+      last_target = event.currentTarget;
+      focus_variable = 0;
+
 });
 }
 }
-
-
-function createCatCard(){
-  img_num = Math.floor(Math.random() * 4); // Generates random number (0-3) in order to select a random cat img from catImage array
-  var catHTML = Handlebars.templates.catCard({
-    catID: "cat" + cat_tracker,
-    catNUM: cat_tracker,
-    age: 0,
-    photoURL: kittenImgURL[img_num],
-    color: img_num,
-    feedStat: 0,
-    groomStat: 0,
-    playStat: 0,
-    total: 0
-  });
-  var cats = document.getElementById('cats');
-  cats.insertAdjacentHTML('beforeend',catHTML);
-  addListToCats();
-}*/
+window.onload = addListToCats();
