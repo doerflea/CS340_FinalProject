d4f1632abb47df04e5dde0d0107d73bec6dad55e
Ryan Little <ryan.jay.little@gmail.com>
2020-05-29 02:26:22 -0700

    Added sessions to remember who's logged in

--- CatteriesPage.js
+++ CatteriesPage.js
@@ -3,10 +3,11 @@ module.exports = function(){
     var router = express.Router();
 
    router.get('/', function(req, res){
-     // SQL command that grabs the cattery name, creator name, and number of cats from
-     // all catteries.
      var mysql = req.app.get('mysql');
-     var sqlcommand = 'SELECT cattery.name catteryName, user.username creatorName, COUNT(cat.id) numCats FROM cattery LEFT JOIN user ON cattery.own_id = user.id JOIN cat ON cat.cattery_id=cattery.id;';
+     // SQL command that grabs the cattery name, creator name, and number of cats from
+     // the user that is logged in.
+     // TODO: Update this SQL command so we also get the catteries a user is visiting.
+     var sqlcommand = 'SELECT cattery.name catteryName, user.id userID, user.username creatorName, COUNT(cat.id) numCats FROM cattery LEFT JOIN user ON cattery.own_id = user.id JOIN cat ON cat.cattery_id=cattery.id WHERE user.id=' + req.session.userID + ';';
      mysql.pool.query(sqlcommand, function(err, rows, fields){
      res.status(200).render('CatteriesPage', {catteryData: rows});
     });
